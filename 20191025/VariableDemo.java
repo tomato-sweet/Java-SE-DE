ba8754211c33531e612913d1083e4749522ee785
tomato-sweet <2528064503@qq.com>
2019-10-27 10:23:20 +0800

    2019/10/25 test

---
+++ 20191025/VariableDemo.java
@@ -0,0 +1,21 @@
+public class VariableDemo{
+	public static void main(String[] args)
+	{
+		// int a;
+		// a = 78;
+		// System.out.println(a);
+		
+		// short minShort = Short.MIN_VALUE;
+		// System.out.println(minShort);
+		
+		// long maxLong = Long.MAX_VALUE;
+		// System.out.println(maxLong);
+		// public class VariableDemo1{
+	
+		// long bLong = 15L;
+		// System.out.println(bLong);
+		//不同类型的赋值
+		
+	}
+	}
+}
