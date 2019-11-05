78a34ad75a0368ae3d3e9e83699345b248c2e1e8
tomato-sweet <2528064503@qq.com>
2019-11-05 15:32:04 +0800

    2019/11/05 RecursionDemo.java

---
+++ 20191105/RecursionDemo.java
@@ -0,0 +1,82 @@
+public class RecursionDemo{
+	//数学归纳法一
+	/*
+	public static int an(int n){
+		if(n == 1){
+			return 1;
+		}else{
+			return an(n-1)+1;
+		}
+	}
+	public static void main(String[] args){
+		for(int i = 1;i <=10;i++){
+			System.out.println(an(i));
+		}
+	}
+	*/
+	
+	/*
+	public static int factorial(int n){
+		if (n == 1){
+			return 1;
+		}else{
+			return factorial(n-1)*n;   //n*an(n-1);
+		}
+	}
+	public static void main(String[] args){
+		for(int i = 1;i <= 10;i++){
+			System.out.println(factorial(i));
+		}
+	}
+	*/
+	
+	//斐波那契数列  1 1 2 3 5 8 13 21 34
+	/*
+	public static long fibonacci(int n){
+		if(n == 0 || n == 1){
+			return 1;
+		}else{
+			return fibonacci(n-1)+fibonacci(n-2);
+		}
+	}
+	public static void main(String[] args){
+		for(int i = 1;i <= 8;i++){
+			System.out.println(fibonacci(i));
+		}
+	}
+	*/
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
