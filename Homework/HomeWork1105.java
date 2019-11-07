0aa2b6e369b07374b553193dda605d26ac7dc6b1
tomato-sweet <2528064503@qq.com>
2019-11-07 11:45:12 +0800

    HomeWork & 20191107

---
+++ Homework/HomeWork1105.java
@@ -0,0 +1,34 @@
+public class HomeWork1105{
+	/*
+	public static void main(String[] args){
+		//编程序数一下1-100的所有整数中出现多少个数字9
+		//当数字模10时，如果余9，则包含数字9（9，19，29，39，49，59，69，79，89，99）
+		//90-99直接包含
+		for(int i=1;i<=100;i++){
+			if(i%10==9 || i/10==9){
+				System.out.println(i);
+			}
+		}
+		*/
+		
+		
+		/*
+		//递归求N的阶乘
+			public static int factorial(int n){
+			if (n == 1){
+				return 1;
+			}else{
+				return factorial(n-1)*n;   //n*an(n-1);
+			}
+		}
+		public static void main(String[] args){
+		Scanner intput = new Scanner(System.in);
+        System.out.println("请输入n的值：");
+        int n = intput.nextInt();
+        int i = factorial(n);
+        System.out.println(i);
+    }
+	*/
+	
+	
+}
