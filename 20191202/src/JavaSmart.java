82b48fce228c73eb90b2dd17f2cd592948c8a6d0
tomato-sweet <2528064503@qq.com>
2019-12-02 14:37:52 +0800

    20191202

---
+++ 20191202/src/JavaSmart.java
@@ -0,0 +1,8 @@
+public class JavaSmart {
+    public static void main(String[] args) {
+        String s = "hello";
+        s += " ";
+        s += "world";
+        System.out.println(s);
+    }
+}
---
+++ 20191202/src/StringDemo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        List<String> courses = new ArrayList<>();
+                courses.add("a");
+                courses.add("ab");
+                courses.add("abc");
+                courses.add("bcd");
+                courses.add("abcd");
+
+    }
+}
