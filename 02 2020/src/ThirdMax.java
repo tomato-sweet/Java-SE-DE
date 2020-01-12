4b8a6468d7b84b5399df124272d04d4724af4090
tomato-sweet <2528064503@qq.com>
2020-01-12 16:40:58 +0800

    study

---
+++ 02 2020/src/ThirdMax.java
@@ -0,0 +1,17 @@
+public class ThirdMax {
+        public int thirdMax(int[] nums) {
+            TreeSet<Integer> tree = new TreeSet();
+
+            for(int i: nums){
+                tree.add(i);
+            }
+
+            if(tree.size() < 3)
+                return tree.last();
+            else{
+                tree.remove(tree.last());
+                tree.remove(tree.last());
+                return tree.last();
+            }
+        }
+    }
