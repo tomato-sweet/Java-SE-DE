6b776cbe2a83d0b31942073b25f06671e623ebe6
tomato-sweet <2528064503@qq.com>
2020-01-23 17:15:57 +0800

    java

---
+++ 02 2020/src/SearchDemo.java
@@ -0,0 +1,32 @@
+/*给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+你的算法时间复杂度必须是 O(log n) 级别。
+ */
+public class SearchDemo {
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int[] targetRange = {-1, -1};
+
+            // find the index of the leftmost appearance of `target`.
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    targetRange[0] = i;
+                    break;
+                }
+            }
+            // if the last loop did not find any index, then there is no valid range
+            // and we return [-1, -1].
+            if (targetRange[0] == -1) {
+                return targetRange;
+            }
+
+            // find the index of the rightmost appearance of `target` (by reverse
+            // iteration). it is guaranteed to appear.
+            for (int j = nums.length-1; j >= 0; j--) {
+                if (nums[j] == target) {
+                    targetRange[1] = j;
+                    break;
+                }
+            }
+            return targetRange;
+        }
+}
