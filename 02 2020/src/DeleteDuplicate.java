1d9f0beff5fcbba0539857cd1bfb792a5315dbd3
tomato-sweet <2528064503@qq.com>
2020-02-02 15:47:39 +0800

    DeleteDuplicate.java

---
+++ 02 2020/src/DeleteDuplicate.java
@@ -0,0 +1,17 @@
+/*
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
+
+public class DeleteDuplicate {
+    public ListNode deleteDuplicates(ListNode head){
+        ListNode current = head;
+        while (current != null && current.next != null){
+            if (current.next.val == current.val){
+                current.next = current.next.next;
+            }else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
