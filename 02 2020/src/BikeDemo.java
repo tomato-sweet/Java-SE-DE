0f1b3e9b67b917dc3656e26ac8fcaf0be364c3fa
tomato-sweet <2528064503@qq.com>
2020-02-04 16:44:55 +0800

    2

---
+++ 02 2020/src/BikeDemo.java
@@ -0,0 +1,28 @@
+public class BikeDemo {
+	public static void main(String[] args) {
+		Bike b = null;
+		b.setColor("hello");
+		/*
+		Bike bike = new Bike();
+		System.out.println("刚初始化:");
+		System.out.println(bike.getColor());
+		System.out.println(bike.getCurrentSpeed());
+		
+		bike.setColor("粉色");
+		System.out.println("涂完漆后:");
+		System.out.println(bike.getColor());
+		
+		System.out.println("下坡:");
+		for (int i = 0; i < 22; i++) {
+			bike.speedUp();
+			System.out.println(bike.getCurrentSpeed());
+		}
+		
+		System.out.println("上坡:");
+		for (int i = 0; i < 22; i++) {
+			bike.speedDown();
+			System.out.println(bike.getCurrentSpeed());
+		}
+		*/
+	}
+}
