db7e675792b05692743281506b86e923e2b17676
tomato-sweet <2528064503@qq.com>
2020-02-07 16:58:42 +0800

    TwoSum.java

---
+++ 02 2020/src/TwoSum.java
@@ -0,0 +1,22 @@
+/*
+给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2
+
+ */
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null) return null;
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[]{i + 1, j + 1};
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return null;
+    }
+}
