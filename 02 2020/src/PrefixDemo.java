9f77cc5851454172eaff3605f19faaa3e9cb348f
tomato-sweet <2528064503@qq.com>
2020-02-08 18:32:35 +0800

    PrefixDemo.java

---
+++ 02 2020/src/PrefixDemo.java
@@ -0,0 +1,22 @@
+/*
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""
+ */
+
+public class PrefixDemo {
+        public String longestCommonPrefix(String[] strs) {
+      if (strs.length == 0)
+          return "";
+      String ans = strs[0];
+      for(int i = 1;i < strs.length;i++){
+          for (int j = 0;j < ans.length() && j < strs[i].length();j++){
+              if (ans.charAt(j) != strs[i].charAt(j))
+                  break;
+          }
+          ans = ans.substring(0,j);
+          if (ans.equals(""))
+              return ans;
+      }
+      return ans;
+    }
+}
