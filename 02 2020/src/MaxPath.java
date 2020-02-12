5806226410aea7fc64892c6ef232f565db088b97
tomato-sweet <2528064503@qq.com>
2020-02-12 15:05:49 +0800

    MaxPath.java

---
+++ 02 2020/src/MaxPath.java
@@ -0,0 +1,13 @@
+/*
+给定一个非空二叉树，返回其最大路径和。
+本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+ */
+public class MaxPath {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
