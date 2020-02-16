7f12ff6ac0d71e48b8b81c855f96b638d04e132a
tomato-sweet <2528064503@qq.com>
2020-02-16 16:58:10 +0800

    CrossLinked.java

---
+++ 02 2020/src/CrossLinked.java
@@ -0,0 +1,14 @@
+/*
+编写一个程序，找到两个单链表相交的起始节点。
+ */
+public class CrossLinked {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
