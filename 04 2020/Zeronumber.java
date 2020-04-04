ebcdbf7d37f9730eb83135386ca278bf4f2abe58
tomato-sweet <2528064503@qq.com>
2020-04-04 11:29:13 +0800

    2

---
+++ 04 2020/Zeronumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+输入一个正整数n,求n!(即阶乘)末尾有多少个0？ 比如: n = 10; n! = 3628800,所以答案为2
+ */
+public class Zeronumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res = 0;
+        for(int i = n; i >= 5; i--) {
+            int tmp = i;
+            while(tmp % 5 == 0) {
+                res++;
+                tmp /= 5;
+            }
+        }
+        System.out.println(res);
+    }
+}
