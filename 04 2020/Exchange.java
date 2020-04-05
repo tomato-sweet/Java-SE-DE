795f0649db02e944f0cc4e91ca57a684cbeafafa
tomato-sweet <2528064503@qq.com>
2020-04-05 11:25:52 +0800

    1

---
+++ 04 2020/Exchange.java
@@ -0,0 +1,9 @@
+
+public class Exchange {
+    public int[] exchangeAB(int[] AB) {
+        AB[0] = AB[0]^AB[1];
+        AB[1] = AB[0]^AB[1];
+        AB[0] = AB[0]^AB[1];
+        return AB;
+    }
+}
