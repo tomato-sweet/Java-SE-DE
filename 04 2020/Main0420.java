b6fbb6abfc8c4b0b5c2d5c3ece5df4de9c6c57af
tomato-sweet <2528064503@qq.com>
2020-04-20 16:43:26 +0800

    1

---
+++ 04 2020/Main0420.java
@@ -0,0 +1,25 @@
+/*
+字串判断
+
+现有一个小写英文字母组成的字符串s和一个包含较短小写英文字符串的数组p，
+请设计一个高效算法，对于p中的每一个较短字符串，判断其是否为s的子串。
+给定一个string数组p和它的大小n，同时给定string s，为母串，请返回一个bool数组，
+每个元素代表p中的对应字符串是否为s的子串。
+保证p中的串长度小于等于8，且p中的串的个数小于等于500，
+同时保证s的长度小于等于1000。
+ */
+
+public class Main0420 {
+    public boolean[] chkSubStr(String[] p, int n, String s){
+        boolean[] res = new boolean[n];
+        for(int i=0;i<p.length;i++){
+            res[i] = isSubStr(p[i],s);
+        }        return res;
+    }
+        public boolean isSubStr(String sub,String s){
+        if(s.indexOf(sub) != -1)
+            return true;
+        else
+            return false;
+    }
+}
